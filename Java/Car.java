ec661c603f1020c33d7aff777669fbc8845f0b05
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-12-03 08:19:46 -0600

    encapsulamiento

--- Java/Car.java
+++ Java/Car.java
@@ -5,14 +5,55 @@ public class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passenger;
+    private Integer passenger;
 
     public Car(String license, Account driver) {
         this.license = license;
         this.driver = driver;
+        this.passenger = 4;
+        System.out.println("passenger: " + passenger);
     }
-    
+
     void printDataCar() {
-        System.out.println("License: " + license + " Driver: " + driver.name);
+        boolean error = false;
+        String mensaje = "";
+        String coma = "";
+        if (this.license == null) {
+            mensaje += coma + "Agregar licencia";
+            coma = ", ";
+            error = true;
+        }
+        if (this.driver == null) {
+            mensaje += coma + "Agregar conductor";
+            coma = ", ";
+            error = true;
+        }
+        if (this.driver != null && this.driver.name == null) {
+            mensaje += coma + "Agregar nombre de conductor";
+            coma = ", ";
+            error = true;
+        }
+        if (this.passenger == null) {
+            mensaje += coma + "Agregar pasajeros";
+            coma = ", ";
+            error = true;
+        }
+        if (error) {
+            System.out.println(mensaje);
+            return;
+        }
+        System.out.println("License: " + license + " Driver: " + driver.name + " Passengers: " + passenger);
+    }
+
+    public Integer getPassenger() {
+        return this.passenger;
+    }
+
+    public void setPassenger(Integer passenger) {
+        if (passenger != null && passenger != 4) {
+            System.out.println("Necesitas asignar 4 pasajeros");
+            return;
+        }
+        this.passenger = passenger;
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -3,12 +3,10 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("hola");
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
-        car.passenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Spark");
+        uberX.printDataCar();
 
         Car car2 = new Car("WQE567", new Account("Andrea Herrera", "ANDA876"));
-        car2.passenger = 3;
         car2.printDataCar();
     }
 }
