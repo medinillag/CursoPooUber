a1d6af317e3ae30f385d0e1d6557b25395285360
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-12-03 07:59:55 -0600

    herencia javascript

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Andres Herrera", "ASD123"));
-car.passenter = 4;
+car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("A2456", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark");
+uberX.passenter = 4;
+uberX.printDataCar();
